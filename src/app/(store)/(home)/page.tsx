f8b4941c761c90761c7036bce5e06824028fa794
nathancamolez-dev <nathan.camolez@unesp.br>
2025-05-16 22:00:51 -0300

    feat: home cache strategy
    Using next: revalidate on the home page, the ideia is that this
    e-commerce is not commum chaning products, so can be a large time of
    cached information.

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api('/products/featured')
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
 
   const products = await response.json()
 
@@ -12,16 +16,16 @@ async function getFeaturedProducts(): Promise<Product[]> {
 }
 
 export default async function Home() {
-  const [highlightedProducts, ...otherProducts] = await getFeaturedProducts()
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts()
   console.log(otherProducts)
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/products/${highlightedProducts.slug}`}
+        href={`/products/${highlightedProduct.slug}`}
         className="relative group col-span-5 row-span-6 rounded-lg bg-zinc-50 overflow-hidden flex  justify-center items-end"
       >
         <Image
-          src={highlightedProducts.image}
+          src={highlightedProduct.image}
           className="group-hover:scale-105 transition-transform duration-500"
           width={920}
           height={920}
@@ -30,9 +34,9 @@ export default async function Home() {
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-zinc-50/60 p-1 pl-5">
-          <span className="text-sm truncate">{highlightedProducts.title}</span>
+          <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-zinc-100 px-4 font-semibold">
-            {highlightedProducts.price.toLocaleString('pt-BR', {
+            {highlightedProduct.price.toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
             })}
