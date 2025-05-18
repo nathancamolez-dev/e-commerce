c17dcad9065f0f945d926a542355fbfe58c1a7cd
nathancamolez-dev <nathan.camolez@unesp.br>
2025-05-17 21:23:39 -0300

    feat: removing the timeout that was for testing
    removing timeout on the the product api , that was used for creating the
    skeleton loading of the page.

--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -4,8 +4,6 @@ export async function GET(
   _: Request,
   { params }: { params: { slug: string } }
 ) {
-  await new Promise(resolve => setTimeout(resolve, 1000))
-
   const slug = z.string().parse(params.slug)
 
   const productDetails = data.products.find(product => product.slug === slug)
