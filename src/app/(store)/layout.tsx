2ef27a5903040b8d586bf6eeccd81af48164c40b
nathancamolez-dev <nathan.camolez@unesp.br>
2025-05-19 19:18:25 -0300

    feat: context for cart
    when differents components are having some behaviour, communicating with
    each other, in react the common way of do it is using context, to create
    a context that this components will be part of.

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
 import { Header } from '@/components/header'
+import { CartProviver } from '@/contexts/cart-context'
 import type { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 px-8 py-8">
-      <Header />
-      {children}
-    </div>
+    <CartProviver>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 px-8 py-8">
+        <Header />
+        {children}
+      </div>
+    </CartProviver>
   )
 }
---
+++ src/contexts/cart-context.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { createContext, useContext, useState } from 'react'
+
+interface CartItem {
+  productId: string
+  quantity: number
+}
+
+interface CartContextType {
+  items: CartItem[]
+  addToCart: (productId: string) => void
+}
+
+const CartContext = createContext({} as CartContextType)
+
+export function CartProviver({ children }: { children: React.ReactNode }) {
+  const [cartItem, setCartItem] = useState<CartItem[]>([])
+
+  function addToCart(productId: string) {
+    setCartItem(state => {
+      const productInCart = state.some(item => item.productId === productId)
+
+      if (productInCart) {
+        return state.map(item => {
+          if (item.productId === productId) {
+            return { ...item, quantity: item.quantity + 1 }
+          }
+          return item
+        })
+      }
+      return [...state, { productId, quantity: 1 }]
+    })
+  }
+
+  return (
+    <CartContext.Provider value={{ items: cartItem, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCart = () => useContext(CartContext)
