8b0cf5fc23b75a62e834e6bdba46f0d18679466a
nathancamolez-dev <nathan.camolez@unesp.br>
2025-07-01 18:54:26 -0300

    feat: home page now is fecthing of the data base API

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
         className="relative group col-span-5 row-span-6 rounded-lg bg-zinc-50 overflow-hidden flex  justify-center items-end"
       >
         <Image
-          src={highlightedProduct.image}
+          src={highlightedProduct.image_url}
           className="group-hover:scale-105 transition-transform duration-500"
           width={920}
           height={920}
@@ -55,7 +55,7 @@ export default async function Home() {
             className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-50 overflow-hidden flex  justify-center items-end"
           >
             <Image
-              src={product.image}
+              src={product.image_url}
               className="group-hover:scale-105 transition-transform duration-500"
               width={920}
               height={920}
